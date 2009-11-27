57c6f5390f54362a25221215662f65d5e3c5b8f7
Jim Morris <morris@athlon>
2009-11-27 14:45:40 -0800

    Added an alignment to align lists and maps in groovy, These get seen as arglists by cc-mode

--- groovy-mode.el
+++ groovy-mode.el
@@ -396,6 +396,29 @@ need for `java-font-lock-extra-types'.")
                 0))
         ))
 
+;; A helper function from: http://mihai.bazon.net/projects/emacs-javascript-mode/javascript.el
+;; Originally named js-lineup-arglist, renamed to groovy-lineup-arglist
+(defun groovy-lineup-arglist (langelem)
+  ;; the "DWIM" in c-mode doesn't Do What I Mean.
+  ;; see doc of c-lineup-arglist for why I redefined this
+  (save-excursion
+    (let ((indent-pos (point)))
+      ;; Normal case.  Indent to the token after the arglist open paren.
+      (goto-char (c-langelem-2nd-pos c-syntactic-element))
+      (if (and c-special-brace-lists
+               (c-looking-at-special-brace-list))
+          ;; Skip a special brace list opener like "({".
+          (progn (c-forward-token-2)
+                 (forward-char))
+        (forward-char))
+      (let ((arglist-content-start (point)))
+        (c-forward-syntactic-ws)
+        (when (< (point) indent-pos)
+          (goto-char arglist-content-start)
+          (skip-chars-forward " \t"))
+        (vector (current-column))))))
+
+
 
 ;;; The entry point into the mode
 (defun groovy-mode ()
@@ -427,6 +450,12 @@ Key bindings:
   (c-set-offset 'statement-cont 0)
   ;; fix for indentation after a closure param list
   (c-set-offset 'brace-list-entry 'groovy-mode-fix-brace-list)
+
+  ;; get arglists (in groovy lists or maps) to align properly
+  (c-set-offset 'arglist-close '(c-lineup-close-paren))
+  (c-set-offset 'arglist-cont 0)
+  (c-set-offset 'arglist-cont-nonempty '(groovy-lineup-arglist))
+  (c-set-offset 'arglist-intro '+)
 
   (c-update-modeline))
 
