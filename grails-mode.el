50801257d376fd7383ddf9c19ff567183c24ad0b
Russel Winder <russel@winder.org.uk>
2016-05-04 10:11:47 +0100

    Relicence from GLPv2 to GPLv3. cf Issue #18.

--- grails-mode.el
+++ grails-mode.el
@@ -5,27 +5,23 @@
 ;; Author: Jim Morris <morris@wolfman.com>
 ;; URL: http://blog.wolfman.com
 ;; Maintainer:  Russel Winder <russel@winder.org.uk>
-;; Version: 201512060749
+;; Version: 201605040908
 ;; Keywords: languages
 
-;;;; NB Version number is date and time yyyymmddhhMM in GMT (aka UTC).
+;;;; NB Version number is date and time yyyymmddhhMM UTC.
 
-;; This file is not currently part of GNU Emacs.
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2, or (at
-;; your option) any later version.
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
 ;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
 ;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program ; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; depends on
 ;; groovy-mode
