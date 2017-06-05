539220fe6dec89ccc4e697cc5f869904fc325179
Wilfred Hughes <me@wilfred.me.uk>
2017-06-05 20:44:43 +0100

    Add a unit test for existing indent logic

--- test/unit-test.el
+++ test/unit-test.el
@@ -1,7 +1,28 @@
 (require 'ert)
+(require 'shut-up)
 (require 'groovy-mode)
 
 (ert-deftest groovy-smoke-test ()
   "Ensure that we can activate the Groovy major mode."
   (with-temp-buffer
     (groovy-mode)))
+
+
+(defmacro should-indent (source result)
+  "Assert that SOURCE is indented to produce RESULT."
+  `(with-temp-buffer
+     (insert ,source)
+     (groovy-mode)
+     (shut-up
+       (indent-region (point-min) (point-max)))
+     (should (equal (buffer-string) ,result))))
+
+(ert-deftest groovy-indent-function ()
+  "We should indent according to the number of parens."
+  (should-indent
+   "def foo() {
+bar()
+}"
+   "def foo() {
+    bar()
+}"))
