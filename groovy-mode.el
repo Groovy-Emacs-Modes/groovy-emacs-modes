d7b362e6186d263ec3eefc141dbb5b27a8773f24
Wilfred Hughes <me@wilfred.me.uk>
2017-12-30 17:30:04 +0000

    Remove commentary which is no longer true of 2.0

--- groovy-mode.el
+++ groovy-mode.el
@@ -31,12 +31,6 @@
 ;;   (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
 ;;   (add-to-list 'auto-mode-alist '("\\.groovy\\'" . groovy-mode))
 
-;;; Commentary:
-;;  This mode was initially developed using the Java and Awk modes that are part of CC Mode (the 5.31 source
-;;  was used) and C# Mode from Dylan R. E. Moonfire <contact@mfgames.com> (the 0.5.0 source was used).  This
-;;  code may contain some code fragments from those sources that was cut-and-pasted then edited.  All other
-;;  code was newly entered by the author.  Obviously changes have been made since then.
-
 ;;; Bugs:
 ;;  Bug tracking is currently handled using the GitHub issue tracker at
 ;;  https://github.com/Groovy-Emacs-Modes/groovy-emacs-modes/issues
